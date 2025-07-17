b6afbd4ef2d59cda396d37cf3c866d6c2a600510
KAVINT21 <95117554+KAVINT21@users.noreply.github.com>
2025-07-17 16:01:14 +0530

    Update stock_market.py

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,27 +1,12 @@
-from airflow import dag,task
+from airflow.decorators import dag, task
 from datetime import datetime
-import requests
 
-def fetch_stock():
-    url = "https://www.alphavantage.co/query"
-    params = {
-        "function": "TIME_SERIES_DAILY",
-        "symbol": "AAPL",
-        "apikey": "1CCQTRMYE2PGX51N"  # 👈 use your key
-    }
-    response = requests.get(url, params=params)
-    print(response.json())  # For logs
-
-@dag(
-    start_date=datetime(2024, 1, 1),
-    schedule='@daily',
-    catchup=False,
-    tags=["stock_market"],
-)
-
-def stock_market():
+@dag(start_date=datetime(2024, 1, 1), schedule_interval="@daily", catchup=False)
+def stock_pipeline():
     @task
-    def fetch_stock_data():
-        fetch_stock()
-    
-stock_market()
+    def start():
+        print("Pipeline started")
+
+    start()
+
+stock_pipeline()
