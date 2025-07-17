2b913fa10c9e5ae8e81487d9b0a548b0a69ec743
KAVINT21 <95117554+KAVINT21@users.noreply.github.com>
2025-07-17 17:20:00 +0530

    Update stock_market.py

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -2,17 +2,15 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-def dummy_task():
-    print("This is a placeholder")
+def print_hello():
+    print("Hello from Astro Cloud!")
 
-with DAG(
-    dag_id="stock_market_pipeline",
-    schedule_interval="@daily",
-    start_date=datetime(2024, 1, 1),
-    catchup=False
-) as dag:
-
-    task1 = PythonOperator(
-        task_id="dummy",
-        python_callable=dummy_task
+with DAG(dag_id="stock_dag",
+         start_date=datetime(2024, 1, 1),
+         schedule_interval="@daily",
+         catchup=False) as dag:
+    
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=print_hello
     )
